07d54f0c443af57434547005fb71660b74398e13
Nikhil334 <nikhil.gupta@appinventiv.com>
2023-07-25 00:56:24 +0530

    adding the axios functionality and using apis of anther project

---
+++ src/controllers/user.axiosinsert.controller.ts
@@ -0,0 +1,37 @@
+import { responseEncoding } from "axios";
+import { insertdatabyaxios, axiosphotodisplay } from "../services/user.axiosinsertservice";
+import { axioslogin } from "../services/user.axiosinsertservice";
+import { Request, Response } from "express";
+
+
+const insertdatacontroller = async (req: Request, res: Response) => {
+    try {
+        const result = await insertdatabyaxios(req);
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+const axioslogincontroller = async (req: Request, res: Response) => {
+    try {
+        const result = await axioslogin(req);
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+const axiosphotocontroller = async (req: Request, res: Response) => {
+    try {
+        const result = await axiosphotodisplay(req);
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export { insertdatacontroller, axioslogincontroller,axiosphotocontroller };
